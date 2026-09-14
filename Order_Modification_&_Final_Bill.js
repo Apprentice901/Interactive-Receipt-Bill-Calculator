3dcc0e7bef306d96981ff84d143df9d1ea694a01
Apprentice901 <defrankkenneth111@gmail.com>
2026-09-13 19:55:27 -0500

    Phase 4: Attempted but failed. I Added a sequence.pop into the array but didn't get far.

---
+++ Order_Modification_&_Final_Bill.js
@@ -0,0 +1,69 @@
+// Phase 4 (Order Modification & Final Bill)
+
+let storeInfo = {
+    name: "", // Variable Values
+    taxRate: 0.08, // Variable Values
+    tableFee: "" // Variable Values
+};
+for (;;) {
+    try {
+        let name = promtDirection("Store Name?: ");
+        let taxRate = promtDirection("Tax Rate?: ")
+        let tableFee = promtDirection("Table?: ")
+        
+        if (!name || name.trim() ==="") throw new Error("Invalid stor name"); // If statement (With Error Type)
+        if (Number.isNaN(taxRate)) throw new Error("Invalid tax rate"); // If statement (With Error Type)
+        if (Number.isNaN(tableFee)) throw new Error("Invalid table fee"); // If statement (With Error Type)
+
+        storeInfo.name = name; // (Object.create) Instances
+        storeInfo.taxRate = taxRate; // (Object.create) Instances
+        storeInfo.tableFee = tableFee; // (Object.create) Instances
+        
+        console.log("Welcome to " + name + ".");
+        console.log("Your Tax Rate is " + taxRate + ".");
+        console.log("Your service fee is " + tableFee + ".");
+        break;
+        } catch (Error) {
+        console.log("Not a valid name. Try again." + name);
+        console.log("Not a valid table. Try again." + tableFee);
+        console.log("Not a valid tax. Try again." + taxRate);
+        }
+    }
+
+receiptItems.prototype.forEach.call({
+// Now that items are collected, process the order line-by-line using a loop.
+let subtotal = 0; 
+receiptItems.prototype.forEach.call({ 
+
+// Process each item in receiptItems one by one
+for (let entry of receiptItems) { // Errors
+    console.log(`${entry.name} -- $${entry.price.toFixed(2)}`);
+
+    subtotal += entry.price;
+
+    console.log(`${entry.events.length} events.`);
+}
+
+    console.log(`Total food subtotal: $${subtotal.toFixed(2)}`); // Error's
+
+    let sequence = [1, 2, 3];
+    sequence.push(4);
+    sequence.push(5);
+    console.log(sequence.pop());
+
+}; console.log(receiptItems));
+console.log (itemName, price);
+
+console.log(subtotal);
+}, console.log(receiptItems));
+
+let newItem; 
+do { // Prompt()
+    newItem = prompt("Do you want a new Item? ((Y)to continue): ");
+} while (!newItem); {
+    itemName = prompt("Enter item Name: ");
+    itemPrice = Number(prompt("Enter item Price: "))
+    let itemObject = {name: "Burger", price: 10.50};
+    itemObject.push(receiptItems);
+}
+console.log(newItem);
